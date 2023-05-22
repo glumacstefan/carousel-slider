82b6ac8756ec013fc3095df1c6764a23191498a9
Stefan Glumac <stefanactor94@gmail.com>
2023-05-22 23:35:01 +0200

    Format JS

--- js/main.js
+++ js/main.js
@@ -5,51 +5,49 @@ window.onload = function () {
    const prev = document.querySelector('.c-carousel__nav-arrow--left');
    const slideWidth = slides[0].getBoundingClientRect().width;
 
+   let currSlide = 0;
+   let firstSlide = slides[0];
+   let lastSlide = slides[slides.length - 1];
 
-    let currSlide = 0;
-    let firstSlide = slides[0];
-    let lastSlide = slides[slides.length - 1];
+   prev.disabled = true;
 
-    prev.disabled = true;
+   function slideLeft() {
+      next.disabled = false;
+      currSlide--;
 
-    function slideLeft() {
-       next.disabled = false;
-       currSlide--;
+      slides.forEach((slide) => {
+         slide.style.transition = 'transform 0.2s linear';
+         slide.style.transform = `translateX(-${currSlide * slideWidth}px)`;
+      });
 
-       slides.forEach((slide) => {
-          slide.style.transition = 'transform 0.2s linear';
-          slide.style.transform = `translateX(-${currSlide * slideWidth}px)`;
-       });
+      if (firstSlide.getBoundingClientRect().left >= 0) {
+         prev.disabled = true;
+      }
+   }
 
-       if (firstSlide.getBoundingClientRect().left >= 0) {
-          prev.disabled = true;
-       }
-    }
+   function slideRight() {
+      prev.disabled = false;
+      currSlide++;
 
-    function slideRight() {
-       prev.disabled = false;
-       currSlide++;
+      slides.forEach((slide) => {
+         slide.style.transition = 'transform 0.2s linear';
+         slide.style.transform = `translateX(-${currSlide * slideWidth}px)`;
+      });
 
-       slides.forEach((slide) => {
-          slide.style.transition = 'transform 0.2s linear';
-          slide.style.transform = `translateX(-${currSlide * slideWidth}px)`;
-       });
+      if (lastSlide.getBoundingClientRect().right <= carousel.getBoundingClientRect().right) {
+         next.disabled = true;
+      }
+   }
 
-       if (lastSlide.getBoundingClientRect().right <= carousel.getBoundingClientRect().right) {
-          next.disabled = true;
-       }
-    }
+   prev.addEventListener('click', slideLeft);
+   next.addEventListener('click', slideRight);
 
-    prev.addEventListener('click', slideLeft);
-    next.addEventListener('click', slideRight);
-
-
-    window.addEventListener('keydown', function (event) {
-       let key = event.key;
-       if (key === 'ArrowLeft' && prev.disabled !== true) {
-          slideLeft();
-       } else if (key === 'ArrowRight' && next.disabled !== true) {
-          slideRight();
-       }
-    }); 
+   window.addEventListener('keydown', function (event) {
+      let key = event.key;
+      if (key === 'ArrowLeft' && prev.disabled !== true) {
+         slideLeft();
+      } else if (key === 'ArrowRight' && next.disabled !== true) {
+         slideRight();
+      }
+   });
 };
